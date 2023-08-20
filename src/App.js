b9c8d754eeb74ca066bac09c3d15726db899258d
prathmesh_khandare <prathameshkhandare1551@gmail.com>
2023-08-20 19:07:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+
+import Inputform from './components/Inputform';
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import Alertcomp from './components/Alertcomp';
+
+
+
+
 
 function App() {
+const [mode, setmode] = useState("light");
+const [alert, setalert] = useState(null)
+
+
+
+
+const showAlert =(message,type)=>{
+
+setalert({
+  msg:message ,
+  type:type
+})
+setTimeout(() => {
+  setalert(null)
+}, 1500);
+
+}
+
+
+
+
+
+
+
+const toggleMode =() =>{
+  if (mode==="light") {
+    setmode("dark");
+    document.body.style.backgroundColor='#042743' ;
+    document.body.style.color='white' ;
+    showAlert("dark mode has been enabled","success")
+  }
+  else{
+    setmode("light");
+    document.body.style.backgroundColor='white';
+    document.body.style.color='black' ;
+    showAlert("light mode has been enabled","success")
+  }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+{/* below title=prathmesh is used as a prop */}
+
+<Navbar title='TextMan' mode={mode} toggleMode={toggleMode}/>   
+
+<Alertcomp alert={alert}/>
+<div className="container-md my-3  "  >
+
+<Inputform showAlert={showAlert} heading="Enter The Text To Analyze below"/> 
+{/* <About/> */}
+
+
+
+</div>
+
+
+
+  </>
   );
 }
 
